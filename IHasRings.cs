c035dfc252536ed374151a2510e2b7dd7a1fe252
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-07-25 11:43:08 -0400

    Added a new interface named IHasRings

---
+++ IHasRings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name - Gurmanpreet Kaur 
+ * Date - 25 July , 2017
+ * Description - This is the interface IHasRings
+ * Version 0.1 - Created IHasRings interface 
+ * */
+namespace Assignment04_Abstract_Planets
+{
+    /// <summary>
+    /// this is the IHasRings interface
+    /// </summary>
+    public interface IHasRings
+    {
+        bool HasRings();
+    }
+}
