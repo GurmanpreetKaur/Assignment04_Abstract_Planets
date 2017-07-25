edd883207d9cd857a6c99f04c8624565642c32cf
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-07-25 11:51:11 -0400

    Added the Driver class to my program

--- Program.cs
+++ Program.cs
@@ -16,6 +16,20 @@ namespace Assignment04_Abstract_Planets
     {
         static void Main(string[] args)
         {
+            GaintPlanet Gaint = new GaintPlanet("Mercury", 4879, 3.285000000000000000, "solid");
+            Console.WriteLine(Gaint.ToString());
+            Console.WriteLine();
+            TerrestrialPlanet teri = new TerrestrialPlanet("Mercury", 4879, 3.285000000000000000, true);
+            Console.WriteLine(teri.ToString());
+            WaitForAnyKey();
+
         }
+        public static void WaitForAnyKey()
+        {
+            string output = "Please enter any key to continue";
+            Console.WriteLine(output);
+
+        }
+    }
     }
 }
