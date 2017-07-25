bf5c1aacb2fe7c16cd566443a2375211539cdcf9
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-07-25 11:40:25 -0400

    Added interface IHasMoons

---
+++ IHasMoons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name - Gurmanpreet Kaur 
+ * Date - 25 July , 2017
+ * Description - This is the interface IHasMoons
+ * Version 0.1 - Created This interface
+ * */
+namespace Assignment04_Abstract_Planets
+{
+    /// <summary>
+    /// this is the IHasMoons interface
+    /// </summary>
+    public interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
