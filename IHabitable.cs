1cbd4066e4198df2d9aa85b6faf922d5e2d29973
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-07-25 11:45:27 -0400

    Added a new interface IHabitable

---
+++ IHabitable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name - Gurmanpreet Kaur 
+ * Date - 25 July , 2017
+ * Description - This is the interface IHabitable
+ * Version 0.1 - Created IHabitable interface 
+ * */
+namespace Assignment04_Abstract_Planets
+{
+    /// <summary>
+    /// this is the IHabitable interface
+    /// </summary>
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
