16faa7dcf8a53580b30892c48fcf4ebb35096841
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-07-25 08:14:25 -0400

    Added class Diagram and Abstract class to my Project

---
+++ Planet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name - Gurmanpreet Kaur 
+ * Date - 25 July , 2017
+ * Description - This is my Abstarct class called Planet
+ * Version 0.1 - Created Abstarct class 
+ * */
+namespace Assignment04_Abstract_Planets
+{
+    /// <summary>
+    /// This is my Abstract class
+    /// </summary>
+    public class Planet
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name - Gurmanpreet Kaur 
+ * Date - 25 July , 2017
+ * Description - This is my main program(driver class) for the project
+ * Version 0.1 - Created main program 
+ * */
+ 
 namespace Assignment04_Abstract_Planets
 {
     class Program
